819f23d9297245bdc634118d4bdef4edc5c30c6d
GudiVaraprasad <gudi.varaprasad@gmail.com>
2023-10-19 14:06:23 -0400

    Add applyFilter() method in the ExpenseTrackerController.java file

--- expense_tracker/src/controller/ExpenseTrackerController.java
+++ expense_tracker/src/controller/ExpenseTrackerController.java
@@ -1,15 +1,12 @@
 package controller;
 
+import model.*;
 import view.ExpenseTrackerView;
 
 import java.util.List;
 
-
-
-import model.ExpenseTrackerModel;
-import model.Transaction;
 public class ExpenseTrackerController {
-  
+
   private ExpenseTrackerModel model;
   private ExpenseTrackerView view;
 
@@ -37,13 +34,29 @@ public class ExpenseTrackerController {
     if (!InputValidation.isValidCategory(category)) {
       return false;
     }
-    
+
     Transaction t = new Transaction(amount, category);
     model.addTransaction(t);
-    view.getTableModel().addRow(new Object[]{t.getAmount(), t.getCategory(), t.getTimestamp()});
+    view.getTableModel().addRow(new Object[] { t.getAmount(), t.getCategory(), t.getTimestamp() });
     refresh();
     return true;
   }
-  
+
   // Other controller methods
+
+  public List<Transaction> applyFilter(Object filterCondition, String filterBy) {
+    TransactionFilter filter;
+
+    if (filterBy.equals("category")) {
+      filter = new TransactionsFilterByCategory((String) filterCondition);
+    } else if (filterBy.equals("amount")) {
+      // Assuming criteria is an array of {minAmount, maxAmount}
+      double[] range = (double[]) filterCondition;
+      filter = new TransactionsFilterByAmount(range[0], range[1]);
+    } else {
+      throw new IllegalArgumentException("Invalid filter type");
+    }
+
+    return filter.filter(model.getTransactions()); // transactions is your list of transactions
+  }
 }

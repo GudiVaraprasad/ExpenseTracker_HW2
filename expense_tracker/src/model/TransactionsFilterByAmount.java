e0301bdb31cb61f5dba3e4f4e332ab9f9cafa6ff
GudiVaraprasad <gudi.varaprasad@gmail.com>
2023-10-19 14:10:41 -0400

    write backend logic for filter by amount

--- expense_tracker/src/model/TransactionsFilterByAmount.java
+++ expense_tracker/src/model/TransactionsFilterByAmount.java
@@ -2,15 +2,16 @@ package model;
 
 import java.util.ArrayList;
 import java.util.List;
- 
-public class TransactionsFilterByAmount implements TransactionFilter{
+
+public class TransactionsFilterByAmount implements TransactionFilter {
     @Override
+    // overriding method for the interface.
     public List<Transaction> filter(List<Transaction> transactions) {
         List<Transaction> filteredTransactions = new ArrayList<Transaction>();
 
-        for(Transaction transaction : transactions){
-            if(transaction.getAmount() >= this.minAmount && transaction.getAmount() <= this.maxAmount){
-                filteredTransactions.add(transaction);
+        for (Transaction transaction : transactions) {
+            if (transaction.getAmount() >= this.minAmount && transaction.getAmount() <= this.maxAmount) {
+                filteredTransactions.add(transaction); // adding transaction
             }
         }
         return filteredTransactions;
